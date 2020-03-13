ce93abc0b7b759cc6e8257a12cadeb39c7ba6c03
Daniel Davis <daniel.davis@nih.gov>
2020-03-13 15:49:18 -0400

    Another small problem with variable naming

--- build_postgres.sh
+++ build_postgres.sh
@@ -3,7 +3,7 @@
 POSTGRESQL_DIR=postgresql-${POSTGRESQL_VERSION}
 
 echo "Building ${POSTGRESQL_DIR}"
-cd ${POSTGREQSL_DIR}
+cd ${POSTGRESQL_DIR}
 ./configure --prefix `pwd` --without-readline --without-zlib --with-openssl
 make
 make install
